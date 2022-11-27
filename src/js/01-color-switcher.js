ba13d3e3259827b5db740c03e2baee1e1a7904a0
Alexsandr-Dvoretskiy <alexdor72742@gmail.com>
2022-11-27 09:00:31 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,31 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+let intervalID = null;
+
+refs.btnStart.addEventListener('click', startChangeBodyColor);
+refs.btnStop.addEventListener('click', stopChangeBodyColor);
+refs.btnStop.disabled = true;
+
+function startChangeBodyColor(e) {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  intervalID = setInterval(setBodyColor, 1000);
+}
+
+function stopChangeBodyColor(e) {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(intervalID);
+  intervalID = 0;
+}
+
+function setBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const btnStart = document.querySelector('button[data-start]');
-const btnStop = document.querySelector('button[data-stop]');
-const body = document.querySelector('body');
-const colorValSpan = document.querySelector('.color');
-
-let timerId = null;
-
-const backgroundColorSwitcher = function () {
-  body.style.backgroundColor = getRandomHexColor();
-  body.style.color = getRandomHexColor();
-  colorValSpan.textContent = getRandomHexColor();
-};
-
-btnStart.addEventListener('click', () => {
-  timerId = setInterval(backgroundColorSwitcher, 1000);
-  btnStart.disabled = true;
-});
-
-btnStop.addEventListener('click', () => {
-  clearInterval(timerId);
-  btnStart.disabled = false;
-});
